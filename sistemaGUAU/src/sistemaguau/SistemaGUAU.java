d2bbacb03577173c59f596ca973863e6d3be2911
José Sánchez <jose.venados6@gmail.com>
2020-06-23 09:14:44 -0600

    Agregación Forms

--- sistemaGUAU/src/sistemaguau/SistemaGUAU.java
+++ sistemaGUAU/src/sistemaguau/SistemaGUAU.java
@@ -14,7 +14,7 @@ public class SistemaGUAU extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Login/FormLogin.fxml"));
         
         Scene scene = new Scene(root);
         

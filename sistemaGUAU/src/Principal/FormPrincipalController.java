a5530823290b81dfaf68c9062d59fe14f37bc544
José Sánchez <jose.venados6@gmail.com>
2020-06-23 17:12:28 -0600

    Form principal

---
+++ sistemaGUAU/src/Principal/FormPrincipalController.java
@@ -0,0 +1,22 @@
+package Principal;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author José Sánchez
+ */
+public class FormPrincipalController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}

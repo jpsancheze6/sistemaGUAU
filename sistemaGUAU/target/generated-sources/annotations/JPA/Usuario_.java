9656ab83e0ca5e8bae629e05815f76f136f2cdf7
José Sánchez <jose.venados6@gmail.com>
2020-07-13 11:03:44 -0600

    Imagenes principal y font

--- sistemaGUAU/target/generated-sources/annotations/JPA/Usuario_.java
+++ sistemaGUAU/target/generated-sources/annotations/JPA/Usuario_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-07-09T01:01:40")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-07-13T08:41:58")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
